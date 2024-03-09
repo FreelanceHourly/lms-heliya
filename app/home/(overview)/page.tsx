ddc82db7d7af8dfc4f53c4f2de200c35f2f47b79
ilinapradhan <ilina04khushi@gmail.com>
2024-03-09 09:32:31 +0530

    members page

--- app/home/(overview)/page.tsx
+++ app/home/(overview)/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/app/ui/about/page";
+import Becomemember from "@/app/ui/becomemember/page";
 
 export default async function Page() {
   return (
     <main>
       <div className="grid gap-6 ">
         <About />
+        <Becomemember/>
       </div>
     </main>
   );
